f1f11a59b3c1507e4449e4100f06fe2839d2075c
Cleomancy <77933444+Cleomancy@users.noreply.github.com>
2025-05-29 16:02:48 +0000

    Add files via upload

--- rice.sh
+++ rice.sh
@@ -3,22 +3,32 @@
 # My custom larbs.xyz setup.
 # Originally from Luke Smith, fitted for my purposes.
 # - the Cleomancer.
+# MAKE SURE YOU SET YOUR SYSTEM TIME MANUALLY AND CORRECTLY, I REMOVED THE AUTOMATION BECAUSE I DON'T USE NTP.
 # License: GNU GPLv3
-
-## ASUMING THIS SCRIPT IS RUN AFTER A BARE INSTALL OF ARCH WITH NOTHING ON IT. LOGGED IN AS ROOT, NOT A USER ACCOUNT.
-## MAKE SURE TIME IS SET UP PROPERLY! I removed it from the script because I always do that manually.
 
 ### OPTIONS AND VARIABLES ###
 
-dotfiles="https://github.com/Cleomancy/dots.git"
-prereq="https://cleomancy.github.io/progs.txt"
-rssurls="https://landchad.net/rss.xml
-https://artixlinux.org/feed.php \"tech\"
-https://www.archlinux.org/feeds/news/ \"tech\" "
-repo="https://github.com/Cleomancy"
-
+dotfilesrepo="https://github.com/Cleomancy/dots.git"
+progsfile="https://raw.githubusercontent.com/Cleomancy/Cleomancy.github.io/refs/heads/master/progs.csv"
+aurhelper="yay"
+repobranch="main"
 export TERM=ansi
+
+rssurls="https://lukesmith.xyz/rss.xml
+https://videos.lukesmith.xyz/feeds/videos.xml?videoChannelId=2 \"~Luke Smith (Videos)\"
+https://notrelated.xyz/rss
+https://www.youtube.com/feeds/videos.xml?channel_id=UCD6VugMZKRhSyzWEWA9W2fg
+https://www.youtube.com/feeds/videos.xml?channel_id=UClOGLGPOqlAiLmOvXW5lKbw
+https://artixlinux.org/feed.php \"tech\"
+https://www.archlinux.org/feeds/news/ \"tech\"
+https://news.opensuse.org/feed.xml \"tech\
+https://www.alpinelinux.org/atom.xml \"tech\" "
+
 ### FUNCTIONS ###
+
+installpkg() {
+	pacman --noconfirm --needed -S "$1" >/dev/null 2>&1
+}
 
 error() {
 	# Log to stderr and exit with failure.
@@ -27,9 +37,12 @@ error() {
 }
 
 welcomemsg() {
-	whiptail --title "Welcome!" --msgbox "This script will install arch support for artix, the AUR helper yay and the required dependencies and programs to accompany my forks of the suckless tools dwm, st, dmenu and dwmblocks." 10 70
+	whiptail --title "Welcome!" \
+		--msgbox "Welcome to Luke's Auto-Rice Bootstrapping Script!\\n\\nThis script will automatically install a fully-featured Linux desktop, which I use as my main machine.\\n\\n-Cleo" 10 60
 
-	whiptail --title "Important Note!" --yes-button "Let's go" --no-button "Cancel" --yesno "Be sure you are on a fresh install of Artix Linux with time set correctly and that you are logged in as root, you will be prompted to create a new user during this script." 10 70
+	whiptail --title "Important Note!" --yes-button "All ready!" \
+		--no-button "Return..." \
+		--yesno "Be sure the computer you are using has current pacman updates and refreshed Arch keyrings.\\n\\nMAKE SURE TIME IS SET UP CORRECTLY! I removed it from the script." 8 70
 }
 
 getuserandpass() {
@@ -47,12 +60,28 @@ getuserandpass() {
 	done
 }
 
+usercheck() {
+	! { id -u "$name" >/dev/null 2>&1; } ||
+		whiptail --title "WARNING" --yes-button "CONTINUE" \
+			--no-button "No wait..." \
+			--yesno "The user \`$name\` already exists on this system. LARBS can install for a user already existing, but it will OVERWRITE any conflicting settings/dotfiles on the user account.\\n\\nLARBS will NOT overwrite your user files, documents, videos, etc., so don't worry about that, but only click <CONTINUE> if you don't mind your settings being overwritten.\\n\\nNote also that LARBS will change $name's password to the one you just gave." 14 70
+}
+
+preinstallmsg() {
+	whiptail --title "Let's get this party started!" --yes-button "Let's go!" \
+		--no-button "No, nevermind!" \
+		--yesno "The rest of the installation will now be totally automated, so you can sit back and relax.\\n\\nIt will take some time, but when done, you can relax even more with your complete system.\\n\\nNow just press <Let's go!> and the system will begin installation!" 13 60 || {
+		clear
+		exit 1
+	}
+}
+
 adduserandpass() {
 	# Adds user `$name` with password $pass1.
 	whiptail --infobox "Adding user \"$name\"..." 7 50
 	useradd -m -g wheel -s /bin/zsh "$name" >/dev/null 2>&1 ||
 		usermod -a -G wheel "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
-    export repodir="/home/$name/.local/src"
+	export repodir="/home/$name/.local/src"
 	mkdir -p "$repodir"
 	chown -R "$name":wheel "$(dirname "$repodir")"
 	echo "$name:$pass1" | chpasswd
@@ -78,64 +107,122 @@ Include = /etc/pacman.d/mirrorlist-arch" >>/etc/pacman.conf
 	esac
 }
 
-yayinstall() {
-	# Installs yay manually. Used only for AUR helper here.
+manualinstall() {
+	# Installs $1 manually. Used only for AUR helper here.
 	# Should be run after repodir is created and var is set.
-	whiptail --infobox "Installing yay manually." 7 50
-	sudo -u "$name" git -C "$repodir" clone https://aur.archlinux.org/yay.git >/dev/null 2>&1 ||
+	pacman -Qq "$1" && return 0
+	whiptail --infobox "Installing \"$1\" manually." 7 50
+	sudo -u "$name" mkdir -p "$repodir/$1"
+	sudo -u "$name" git -C "$repodir" clone --depth 1 --single-branch \
+		--no-tags -q "https://aur.archlinux.org/$1.git" "$repodir/$1" ||
 		{
-			cd "$repodir/yay" || return 1
-			sudo -u "$name" git pull --force origin master >/dev/null 2>&1
+			cd "$repodir/$1" || return 1
+			sudo -u "$name" git pull --force origin master
 		}
-	cd "$repodir/yay" || exit 1
+	cd "$repodir/$1" || exit 1
 	sudo -u "$name" \
 		makepkg --noconfirm -si >/dev/null 2>&1 || return 1
 }
 
-installdeps() {
-	([ -f "$prereq" ] && cp "$prereq" /tmp/progs.txt) ||
-		curl -Ls "$prereq" | sed '/^#/d' >/tmp/progs.txt
-	total=$(wc -l </tmp/progs.txt) ## counts the number of lines for file.
-	while IFS=, read -r program; do
-		n=$((n + 1))
-			whiptail --title "installation of dependencies" \
-		--infobox "Installing \`$program\` ($n of $total)." 9 70
-        sudo -u "$name" yay --needed --noconfirm -S "$program" > /dev/null 2>&1
-	done </tmp/progs.txt
-}
-
-installdots() {
-	# Downloads a gitrepo $1 and places the files in $2 only overwriting conflicts
-	whiptail --infobox "Downloading and installing config files..." 7 60
-	dir=$(mktemp -d)
-	chown "$name":wheel "$dir"
-	sudo -u "$name" git -C "$repodir" clone --depth 1 \
-		--single-branch --no-tags -q --recursive -b main \
-		--recurse-submodules "$1" "$dir" > /dev/null 2>&1
-	sudo -u "$name" cp -rfT "$dir" "$2"
+maininstall() {
+	# Installs all needed programs from main repo.
+	whiptail --title "LARBS Installation" --infobox "Installing \`$1\` ($n of $total). $1 $2" 9 70
+	installpkg "$1"
 }
 
 gitmakeinstall() {
-	dir="$repodir/$1"
-	whiptail --title "Glorious DWM Installation" \
-		--infobox "Installing \`$1\` via \`git\` and \`make\`." 8 70
-	sudo -u "$name" git -C "$repodir" clone --depth 1 --single-branch --no-tags -q "$repo/$1" "$dir"
+	progname="${1##*/}"
+	progname="${progname%.git}"
+	dir="$repodir/$progname"
+	whiptail --title "LARBS Installation" \
+		--infobox "Installing \`$progname\` ($n of $total) via \`git\` and \`make\`. $(basename "$1") $2" 8 70
+	sudo -u "$name" git -C "$repodir" clone --depth 1 --single-branch \
+		--no-tags -q "$1" "$dir" ||
+		{
+			cd "$dir" || return 1
+			sudo -u "$name" git pull --force origin master
+		}
 	cd "$dir" || exit 1
 	make >/dev/null 2>&1
 	make install >/dev/null 2>&1
 	cd /tmp || return 1
 }
 
+aurinstall() {
+	whiptail --title "LARBS Installation" \
+		--infobox "Installing \`$1\` ($n of $total) from the AUR. $1 $2" 9 70
+	echo "$aurinstalled" | grep -q "^$1$" && return 1
+	sudo -u "$name" $aurhelper -S --noconfirm "$1" >/dev/null 2>&1
+}
+
+pipinstall() {
+	whiptail --title "LARBS Installation" \
+		--infobox "Installing the Python package \`$1\` ($n of $total). $1 $2" 9 70
+	[ -x "$(command -v "pip")" ] || installpkg python-pip >/dev/null 2>&1
+	yes | pip install "$1"
+}
+
+installationloop() {
+	([ -f "$progsfile" ] && cp "$progsfile" /tmp/progs.csv) ||
+		curl -Ls "$progsfile" | sed '/^#/d' >/tmp/progs.csv
+	total=$(wc -l </tmp/progs.csv)
+	aurinstalled=$(pacman -Qqm)
+	while IFS=, read -r tag program comment; do
+		n=$((n + 1))
+		echo "$comment" | grep -q "^\".*\"$" &&
+			comment="$(echo "$comment" | sed -E "s/(^\"|\"$)//g")"
+		case "$tag" in
+		"A") aurinstall "$program" "$comment" ;;
+		"G") gitmakeinstall "$program" "$comment" ;;
+		"P") pipinstall "$program" "$comment" ;;
+		*) maininstall "$program" "$comment" ;;
+		esac
+	done </tmp/progs.csv
+}
+
+putgitrepo() {
+	# Downloads a gitrepo $1 and places the files in $2 only overwriting conflicts
+	whiptail --infobox "Downloading and installing config files..." 7 60
+	[ -z "$3" ] && branch="master" || branch="$repobranch"
+	dir=$(mktemp -d)
+	[ ! -d "$2" ] && mkdir -p "$2"
+	chown "$name":wheel "$dir" "$2"
+	sudo -u "$name" git -C "$repodir" clone --depth 1 \
+		--single-branch --no-tags -q --recursive -b "$branch" \
+		--recurse-submodules "$1" "$dir"
+	sudo -u "$name" cp -rfT "$dir" "$2"
+}
+
+vimplugininstall() {
+	# Installs vim plugins.
+	whiptail --infobox "Installing neovim plugins..." 7 60
+	mkdir -p "/home/$name/.config/nvim/autoload"
+	curl -Ls "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" >  "/home/$name/.config/nvim/autoload/plug.vim"
+	chown -R "$name:wheel" "/home/$name/.config/nvim"
+	sudo -u "$name" nvim -c "PlugInstall|q|q"
+}
+
+makeuserjs(){
+	# Get the Arkenfox user.js and prepare it.
+	arkenfox="$pdir/arkenfox.js"
+	overrides="$pdir/user-overrides.js"
+	userjs="$pdir/user.js"
+	ln -fs "/home/$name/.config/firefox/larbs.js" "$overrides"
+	[ ! -f "$arkenfox" ] && curl -sL "https://raw.githubusercontent.com/arkenfox/user.js/master/user.js" > "$arkenfox"
+	cat "$arkenfox" "$overrides" > "$userjs"
+	chown "$name:wheel" "$arkenfox" "$userjs"
+}
+
 finalize() {
 	whiptail --title "All done!" \
-		--msgbox "Log out and log back in as your new user. If it works, it works. Make sure to set up cron jobs for the scripts in .local/bin/cron." 13 80
+		--msgbox "Congrats! Provided there were no hidden errors, the script completed successfully and all the programs and configuration files should be in place.\\n\\nTo run the new graphical environment, log out and log back in as your new user, then run the command \"startx\" to start the graphical environment (it will start automatically in tty1).\\n\\n.t Luke" 13 80
 }
 
 ### THE ACTUAL SCRIPT ###
 
 ### This is how everything happens in an intuitive format and order.
 
-## Check if user is root on Arch distro. Install whiptail.
+# Check if user is root on Arch distro. Install whiptail.
 pacman --noconfirm --needed -Sy libnewt ||
 	error "Are you sure you're running this as the root user, are on an Arch-based distribution and have an internet connection?"
 
@@ -145,15 +232,22 @@ welcomemsg || error "User exited."
 # Get and verify username and password.
 getuserandpass || error "User exited."
 
+# Give warning if user already exists.
+usercheck || error "User exited."
+
+# Last chance for user to back out before install.
+preinstallmsg || error "User exited."
+
 ### The rest of the script requires no user input.
 
 # Refresh Arch keyrings.
-refreshkeys || error "Error automatically refreshing Arch keyring. Consider doing so manually."
+refreshkeys ||
+	error "Error automatically refreshing Arch keyring. Consider doing so manually."
 
-for x in curl ca-certificates base-devel git zsh; do
+for x in curl ca-certificates base-devel git zsh dash; do
 	whiptail --title "LARBS Installation" \
 		--infobox "Installing \`$x\` which is required to install and configure other programs." 8 70
-	pacman --noconfirm --needed -S "$x" > /dev/null 2>&1
+	installpkg "$x"
 done
 
 adduserandpass || error "Error adding username and/or password."
@@ -163,7 +257,8 @@ adduserandpass || error "Error adding username and/or password."
 # Allow user to run sudo without password. Since AUR programs must be installed
 # in a fakeroot environment, this is required for all builds with AUR.
 trap 'rm -f /etc/sudoers.d/larbs-temp' HUP INT QUIT TERM PWR EXIT
-echo "%wheel ALL=(ALL) NOPASSWD: ALL" >/etc/sudoers.d/temp
+echo "%wheel ALL=(ALL) NOPASSWD: ALL
+Defaults:%wheel,root runcwd=*" >/etc/sudoers.d/larbs-temp
 
 # Make pacman colorful, concurrent downloads and Pacman eye-candy.
 grep -q "ILoveCandy" /etc/pacman.conf || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
@@ -172,32 +267,32 @@ sed -Ei "s/^#(ParallelDownloads).*/\1 = 3/;/^#Color$/s/#//" /etc/pacman.conf
 # Use all cores for compilation.
 sed -i "s/-j2/-j$(nproc)/;/^#MAKEFLAGS/s/^#//" /etc/makepkg.conf
 
-yayinstall || error "Failed to install AUR helper."
+manualinstall $aurhelper || error "Failed to install AUR helper."
 
 # Make sure .*-git AUR packages get updated automatically.
-yay -Y --save --devel
+$aurhelper -Y --save --devel
 
 # The command that does all the installing. Reads the progs.csv file and
 # installs each needed program the way required. Be sure to run this only after
 # the user has been created and has priviledges to run sudo without a password
 # and all build dependencies are installed.
-installdeps
-
-gitmakeinstall "gdwm"
-gitmakeinstall "gmenu"
-gitmakeinstall "st"
-gitmakeinstall "dwmblocks"
+installationloop
 
 # Install the dotfiles in the user's home directory, but remove .git dir and
 # other unnecessary files.
-installdots "$dotfiles" "/home/$name"
+putgitrepo "$dotfilesrepo" "/home/$name" "$repobranch"
 rm -rf "/home/$name/.git/" "/home/$name/README.md" "/home/$name/LICENSE" "/home/$name/FUNDING.yml"
-
-chmod -R 777 "/home/$name/.local/bin"
 
 # Write urls for newsboat if it doesn't already exist
 [ -s "/home/$name/.config/newsboat/urls" ] ||
-	sudo -u "$name" echo "$rssurls" > "/home/$name/.config/newsboat/urls"
+	echo "$rssurls" | sudo -u "$name" tee "/home/$name/.config/newsboat/urls" >/dev/null
+
+# Install vim plugins if not alread present.
+[ ! -f "/home/$name/.config/nvim/autoload/plug.vim" ] && vimplugininstall
+
+# Most important command! Get rid of the beep!
+rmmod pcspkr
+echo "blacklist pcspkr" >/etc/modprobe.d/nobeep.conf
 
 # Make zsh the default shell for the user.
 chsh -s /bin/zsh "$name" >/dev/null 2>&1
@@ -205,38 +300,56 @@ sudo -u "$name" mkdir -p "/home/$name/.cache/zsh/"
 sudo -u "$name" mkdir -p "/home/$name/.config/abook/"
 sudo -u "$name" mkdir -p "/home/$name/.config/mpd/playlists/"
 
-# Make bash the default #!/bin/sh symlink.
-ln -sfT /bin/bash /bin/sh >/dev/null 2>&1
+# Make dash the default #!/bin/sh symlink.
+ln -sfT /bin/dash /bin/sh >/dev/null 2>&1
 
-# dbus UUID must be generated for Artix runit. I usually leave it commented out as I use OpenRC. It is wise to keep it.
+# dbus UUID must be generated for Artix runit. I usually use openRC but you never know.
 #dbus-uuidgen >/var/lib/dbus/machine-id
 
-### LAPTOP STUFF
+# Use system notifications for Brave on Artix
+# Only do it when systemd is not present
+[ "$(readlink -f /sbin/init)" != "/usr/lib/systemd/systemd" ] && echo "export \$(dbus-launch)" >/etc/profile.d/dbus.sh
 
-# Most important command! Get rid of the beep!
-#rmmod pcspkr
-#echo "blacklist pcspkr" >/etc/modprobe.d/nobeep.conf
-
-# Enable tap to click
-#[ ! -f /etc/X11/xorg.conf.d/40-libinput.conf ] && printf 'Section "InputClass"
-##        Identifier "libinput touchpad catchall"
-#        MatchIsTouchpad "on"
-#        MatchDevicePath "/dev/input/event*"
-#        Driver "libinput"
+# Enable tap to click commented out since I don't use a laptop all the time
+<<COMMENT
+[ ! -f /etc/X11/xorg.conf.d/40-libinput.conf ] && printf 'Section "InputClass"
+        Identifier "libinput touchpad catchall"
+        MatchIsTouchpad "on"
+        MatchDevicePath "/dev/input/event*"
+        Driver "libinput"
 	# Enable left mouse button by tapping
-#	Option "Tapping" "on"
-#EndSection' >/etc/X11/xorg.conf.d/40-libinput.conf
+	Option "Tapping" "on"
+EndSection' >/etc/X11/xorg.conf.d/40-libinput.conf
+COMMENT
+
+# All this below to get Librewolf installed with add-ons and non-bad settings.
+
+whiptail --infobox "Setting browser privacy settings and add-ons..." 7 60
+
+browserdir="/home/$name/.librewolf"
+profilesini="$browserdir/profiles.ini"
+
+# Start librewolf headless so it generates a profile. Then get that profile in a variable.
+sudo -u "$name" librewolf --headless >/dev/null 2>&1 &
+sleep 1
+profile="$(sed -n "/Default=.*.default-default/ s/.*=//p" "$profilesini")"
+pdir="$browserdir/$profile"
+
+[ -d "$pdir" ]
+
+# Kill the now unnecessary librewolf instance.
+pkill -u "$name" librewolf
 
 # Allow wheel users to sudo with password and allow several system commands
 # (like `shutdown` to run without password).
 echo "%wheel ALL=(ALL:ALL) ALL" >/etc/sudoers.d/00-larbs-wheel-can-sudo
-echo "%wheel ALL=(ALL:ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/wifi-menu,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman -Syu,/usr/bin/pacman -Syyu,/usr/bin/pacman -Syyu --noconfirm,/usr/bin/loadkeys,/usr/bin/pacman -Syyuw --noconfirm,/usr/bin/pacman -S -y --config /etc/pacman.conf --,/usr/bin/pacman -S -y -u --config /etc/pacman.conf --" >/etc/sudoers.d/01-larbs-cmds-without-password
+echo "%wheel ALL=(ALL:ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/poweroff,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/wifi-menu,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman -Syu,/usr/bin/pacman -Syyu,/usr/bin/pacman -Syyu --noconfirm,/usr/bin/loadkeys,/usr/bin/pacman -Syyuw --noconfirm,/usr/bin/pacman -S -y --config /etc/pacman.conf --,/usr/bin/pacman -S -y -u --config /etc/pacman.conf --" >/etc/sudoers.d/01-larbs-cmds-without-password
 echo "Defaults editor=/usr/bin/nvim" >/etc/sudoers.d/02-larbs-visudo-editor
 mkdir -p /etc/sysctl.d
 echo "kernel.dmesg_restrict = 0" > /etc/sysctl.d/dmesg.conf
 
 # Cleanup
-rm -f /etc/sudoers.d/temp
+rm -f /etc/sudoers.d/larbs-temp
 
 # Last message! Install complete!
 finalize
